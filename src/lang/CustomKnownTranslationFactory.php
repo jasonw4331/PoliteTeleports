1e39cfb3aac6170be8e3d6cafb25532a8270c973
jasonwynn10 <jasonwynn10@me.com>
2023-02-04 15:07:54 -0500

    Generate translation keys with script

---
+++ src/lang/CustomKnownTranslationFactory.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\PoliteTeleports\lang;
+
+use pocketmine\lang\Translatable;
+
+/**
+ * This class contains factory methods for all the translations known to PoliteTeleports.
+ * This class is generated automatically, do NOT modify it by hand.
+ *
+ * @internal
+ */
+final class CustomKnownTranslationFactory{
+	public static function command_tpaccept_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_DESCRIPTION, []);
+	}
+
+	public static function command_tpaccept_noplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_NOPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpaccept_norequest() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_NOREQUEST, []);
+	}
+
+	public static function command_tpaccept_norequestplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_NOREQUESTPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpaccept_offline() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_OFFLINE, []);
+	}
+
+	public static function command_tpaccept_success() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_SUCCESS, []);
+	}
+
+	public static function command_tpaccept_usage() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPACCEPT_USAGE, []);
+	}
+
+	public static function command_tpahere_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPAHERE_DESCRIPTION, []);
+	}
+
+	public static function command_tpahere_noplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPAHERE_NOPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpahere_success(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPAHERE_SUCCESS, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpahere_usage() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPAHERE_USAGE, []);
+	}
+
+	public static function command_tpask_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPASK_DESCRIPTION, []);
+	}
+
+	public static function command_tpask_noplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPASK_NOPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpask_success(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPASK_SUCCESS, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpask_usage() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPASK_USAGE, []);
+	}
+
+	public static function command_tpconfig_alert_rcv_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_ALERT_RCV_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_alert_tp_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_ALERT_TP_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_boolvalue() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_BOOLVALUE, []);
+	}
+
+	public static function command_tpconfig_countdown_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_COUNTDOWN_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_delay_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_DELAY_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_delay_off() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_DELAY_OFF, []);
+	}
+
+	public static function command_tpconfig_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_header() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_HEADER, []);
+	}
+
+	public static function command_tpconfig_intvalue() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_INTVALUE, []);
+	}
+
+	public static function command_tpconfig_language_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_LANGUAGE_DESCRIPTION, []);
+	}
+
+	public static function command_tpconfig_language_invalid() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_LANGUAGE_INVALID, []);
+	}
+
+	public static function command_tpconfig_novalue() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_NOVALUE, []);
+	}
+
+	public static function command_tpconfig_success(Translatable|string $param0, Translatable|string $param1) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_SUCCESS, [
+			0 => $param0,
+			1 => $param1,
+		]);
+	}
+
+	public static function command_tpconfig_usage() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPCONFIG_USAGE, []);
+	}
+
+	public static function command_tpdeny_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_DESCRIPTION, []);
+	}
+
+	public static function command_tpdeny_noplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_NOPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpdeny_norequest() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_NOREQUEST, []);
+	}
+
+	public static function command_tpdeny_norequestplayer(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_NOREQUESTPLAYER, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpdeny_offline() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_OFFLINE, []);
+	}
+
+	public static function command_tpdeny_success(Translatable|string $param0) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_SUCCESS, [
+			0 => $param0,
+		]);
+	}
+
+	public static function command_tpdeny_usage() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::COMMAND_TPDENY_USAGE, []);
+	}
+
+	public static function language_name() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::LANGUAGE_NAME, []);
+	}
+
+	public static function language_selected(Translatable|string $param0, Translatable|string $param1) : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::LANGUAGE_SELECTED, [
+			0 => $param0,
+			1 => $param1,
+		]);
+	}
+
+}
---
+++ src/lang/CustomKnownTranslationKeys.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\PoliteTeleports\lang;
+
+/**
+ * This class contains constants for all the translations known to PoliteTeleports.
+ * This class is generated automatically, do NOT modify it by hand.
+ *
+ * @internal
+ */
+final class CustomKnownTranslationKeys{
+	public const COMMAND_TPACCEPT_DESCRIPTION = "command.tpaccept.description";
+	public const COMMAND_TPACCEPT_NOPLAYER = "command.tpaccept.noplayer";
+	public const COMMAND_TPACCEPT_NOREQUEST = "command.tpaccept.norequest";
+	public const COMMAND_TPACCEPT_NOREQUESTPLAYER = "command.tpaccept.norequestplayer";
+	public const COMMAND_TPACCEPT_OFFLINE = "command.tpaccept.offline";
+	public const COMMAND_TPACCEPT_SUCCESS = "command.tpaccept.success";
+	public const COMMAND_TPACCEPT_USAGE = "command.tpaccept.usage";
+	public const COMMAND_TPAHERE_DESCRIPTION = "command.tpahere.description";
+	public const COMMAND_TPAHERE_NOPLAYER = "command.tpahere.noplayer";
+	public const COMMAND_TPAHERE_SUCCESS = "command.tpahere.success";
+	public const COMMAND_TPAHERE_USAGE = "command.tpahere.usage";
+	public const COMMAND_TPASK_DESCRIPTION = "command.tpask.description";
+	public const COMMAND_TPASK_NOPLAYER = "command.tpask.noplayer";
+	public const COMMAND_TPASK_SUCCESS = "command.tpask.success";
+	public const COMMAND_TPASK_USAGE = "command.tpask.usage";
+	public const COMMAND_TPCONFIG_ALERT_RCV_DESCRIPTION = "command.tpconfig.alert.rcv.description";
+	public const COMMAND_TPCONFIG_ALERT_TP_DESCRIPTION = "command.tpconfig.alert.tp.description";
+	public const COMMAND_TPCONFIG_BOOLVALUE = "command.tpconfig.boolvalue";
+	public const COMMAND_TPCONFIG_COUNTDOWN_DESCRIPTION = "command.tpconfig.countdown.description";
+	public const COMMAND_TPCONFIG_DELAY_DESCRIPTION = "command.tpconfig.delay.description";
+	public const COMMAND_TPCONFIG_DELAY_OFF = "command.tpconfig.delay.off";
+	public const COMMAND_TPCONFIG_DESCRIPTION = "command.tpconfig.description";
+	public const COMMAND_TPCONFIG_HEADER = "command.tpconfig.header";
+	public const COMMAND_TPCONFIG_INTVALUE = "command.tpconfig.intvalue";
+	public const COMMAND_TPCONFIG_LANGUAGE_DESCRIPTION = "command.tpconfig.language.description";
+	public const COMMAND_TPCONFIG_LANGUAGE_INVALID = "command.tpconfig.language.invalid";
+	public const COMMAND_TPCONFIG_NOVALUE = "command.tpconfig.novalue";
+	public const COMMAND_TPCONFIG_SUCCESS = "command.tpconfig.success";
+	public const COMMAND_TPCONFIG_USAGE = "command.tpconfig.usage";
+	public const COMMAND_TPDENY_DESCRIPTION = "command.tpdeny.description";
+	public const COMMAND_TPDENY_NOPLAYER = "command.tpdeny.noplayer";
+	public const COMMAND_TPDENY_NOREQUEST = "command.tpdeny.norequest";
+	public const COMMAND_TPDENY_NOREQUESTPLAYER = "command.tpdeny.norequestplayer";
+	public const COMMAND_TPDENY_OFFLINE = "command.tpdeny.offline";
+	public const COMMAND_TPDENY_SUCCESS = "command.tpdeny.success";
+	public const COMMAND_TPDENY_USAGE = "command.tpdeny.usage";
+	public const LANGUAGE_NAME = "language.name";
+	public const LANGUAGE_SELECTED = "language.selected";
+}
