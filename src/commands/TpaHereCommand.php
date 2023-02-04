5245796e732f03af32e99023275ffca8bf4eca9b
jasonwynn10 <jasonwynn10@me.com>
2023-02-04 15:13:42 -0500

    translate tpahere

--- src/commands/TpaHereCommand.php
+++ src/commands/TpaHereCommand.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace jasonwynn10\PoliteTeleports\commands;
 
+use jasonwynn10\PoliteTeleports\lang\CustomKnownTranslationFactory;
 use jasonwynn10\PoliteTeleports\Main;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -20,8 +21,8 @@ class TpaHereCommand extends Command implements PluginOwned{
 		$this->setOwningPlugin($owningPlugin);
 		parent::__construct(
 			"tpaskhere",
-			"Send a request to teleport another player to you",
-			"/tpahere <player: target>",
+			CustomKnownTranslationFactory::command_tpahere_description(),
+			CustomKnownTranslationFactory::command_tpahere_usage(),
 			["tpah", "tpahere"]
 		);
 		$this->setPermission('PoliteTeleports.command.tpahere');
@@ -39,10 +40,10 @@ class TpaHereCommand extends Command implements PluginOwned{
 		}
 		$player = $this->owningPlugin->getServer()->getPlayerByPrefix($args[0]);
 		if($player === null) {
-			$sender->sendMessage(TextFormat::RED . "Can't find player " . $args[0]);
+			$sender->sendMessage(CustomKnownTranslationFactory::command_tpahere_noplayer($args[0])->prefix(TextFormat::RED));
 			return;
 		}
 		$this->owningPlugin->addRequest($player->getName(), $sender->getName(), $sender->getName());
-		$sender->sendMessage("Teleport request sent to ".$player->getName());
+		$sender->sendMessage(CustomKnownTranslationFactory::command_tpahere_success($player->getName()));
 	}
 }
