dcc2f73eda66ee326040b9e592158a5b729ebaca
jasonwynn10 <jasonwynn10@me.com>
2023-02-04 02:41:33 -0500

    Added TeleportRequest utility class

---
+++ src/TeleportRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\PoliteTeleports;
+
+final class TeleportRequest{
+	private bool $isCancelled = false;
+
+	public function __construct(
+		private string $fromTarget,
+		private string $toTarget,
+		private string $requester
+	) {}
+
+	public function getFromTarget() : string{
+		return $this->fromTarget;
+	}
+
+	public function getToTarget() : string{
+		return $this->toTarget;
+	}
+
+	public function getRequester() : string{
+		return $this->requester;
+	}
+
+	public function cancel() : void{
+		$this->isCancelled = true;
+	}
+
+	public function isCancelled() : bool{
+		return $this->isCancelled;
+	}
+}
