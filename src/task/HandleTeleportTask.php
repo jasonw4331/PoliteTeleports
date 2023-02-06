41553670d1f41bf6493f6778f8c9d3d6581c091d
jasonwynn10 <jasonwynn10@me.com>
2023-02-06 13:36:42 -0500

    Finish translations in HandleTeleportTask

--- src/task/HandleTeleportTask.php
+++ src/task/HandleTeleportTask.php
@@ -1,8 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 namespace jasonwynn10\PoliteTeleports\task;
 
+use jasonwynn10\PoliteTeleports\lang\CustomKnownTranslationFactory;
 use jasonwynn10\PoliteTeleports\Main;
 use jasonwynn10\PoliteTeleports\TeleportRequest;
 use pocketmine\command\Command;
@@ -10,11 +12,12 @@ use pocketmine\lang\KnownTranslationFactory;
 use pocketmine\scheduler\CancelTaskException;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
+use function ceil;
 
 class HandleTeleportTask extends Task{
 	private int $finalTick;
 
-	public function __construct(private TeleportRequest &$request, int $delayTicks) {
+	public function __construct(private TeleportRequest &$request, int $delayTicks){
 		$this->finalTick = Server::getInstance()->getTick() + $delayTicks;
 	}
 
@@ -25,34 +28,38 @@ class HandleTeleportTask extends Task{
 		$server = Server::getInstance();
 		$requester = $this->request->getRequester();
 		$fromTarget = $server->getPlayerExact($this->request->getFromTarget());
-		if($fromTarget === null) { // player offline
-			$server->getPlayerExact($requester)?->sendMessage("The teleporting player is no longer online");
+		if($fromTarget === null){ // player offline
+			if(Main::getPlayerSettings($this->request->getToTarget())['Alert Receiver'])
+				$server->getPlayerExact($this->request->getToTarget())?->sendMessage(CustomKnownTranslationFactory::teleport_state_tpingoffline());
 			throw new CancelTaskException();
 		}
 		$toTarget = $server->getPlayerExact($this->request->getToTarget());
-		if($toTarget === null) { // player offline
-			$server->getPlayerExact($requester)?->sendMessage("The receiving player is no longer online");
+		if($toTarget === null){ // player offline
+			if(Main::getPlayerSettings($this->request->getFromTarget())['Alert Receiver'])
+				$server->getPlayerExact($this->request->getFromTarget())?->sendMessage(CustomKnownTranslationFactory::teleport_state_rcvoffline());
 			throw new CancelTaskException();
 		}
 
-		if($this->request->isCancelled()) { // player first approved tp then denied while waiting
+		if($this->request->isCancelled()){ // player first approved tp then denied while waiting
 			if(Main::getPlayerSettings($fromTarget->getName())['Alert Teleporting'])
-				$fromTarget->sendMessage("Teleport denied.");
+				$fromTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_cancelled());
 
 			if(Main::getPlayerSettings($toTarget->getName())['Alert Receiver'])
-				$toTarget->sendMessage("Teleport denied.");
+				$toTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_cancelled());
 			throw new CancelTaskException();
 		}
 
 		$tickDiff = $this->finalTick - $server->getTick();
-		if($tickDiff >= 20 and Main::getPlayerSettings($fromTarget->getName())['Teleport Countdown']) {
-			$fromTarget->sendMessage("Teleporting in ".ceil($tickDiff / 20)." second(s)");
+		if($tickDiff >= 20 && Main::getPlayerSettings($fromTarget->getName())['Teleport Countdown']){
+			$fromTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_time(
+				CustomKnownTranslationFactory::teleport_state_seconds()->prefix(ceil($tickDiff / 20) . ' ')
+			));
 			return;
 		}
 
-		if(!$fromTarget->teleport($toTarget->getLocation())) { // likely tp event cancelled by other plugin
+		if(!$fromTarget->teleport($toTarget->getLocation())){ // likely tp event cancelled by other plugin
 			if(Main::getPlayerSettings($fromTarget->getName())['Alert Teleporting'])
-				$fromTarget->sendMessage("Teleport to ".$toTarget->getName()." failed. Retrying...");
+				$fromTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_failed($toTarget->getName()));
 
 			$this->finalTick += $server->getPluginManager()->getPlugin('PoliteTeleports')->getConfig()->get('Retry Interval', 5) * 20; // retry teleport in configured seconds
 			return;
@@ -65,13 +72,13 @@ class HandleTeleportTask extends Task{
 		);
 
 		if(Main::getPlayerSettings($fromTarget->getName())['Alert Teleporting'])
-			$fromTarget->sendMessage("Teleported to ".$toTarget->getName());
+			$fromTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_successfrom($toTarget->getName()));
 
 		if(Main::getPlayerSettings($toTarget->getName())['Alert Receiver'])
-			$toTarget->sendMessage($fromTarget->getName()." has teleported to you");
+			$toTarget->sendMessage(CustomKnownTranslationFactory::teleport_state_successto($fromTarget->getName()));
 
 		$this->request->cancel(); // request can now be removed from the queue
 
-		throw new CancelTaskException;
+		throw new CancelTaskException();
 	}
 }
